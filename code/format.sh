372bda5de0afca85fb9c0c4705b0a921a470bef2
Jason Harris <sirmanlypowers@gmail.com>
2026-08-04 10:50:43 -0700

    Moved driver files into a tec1 library.

---
+++ code/format.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+FILES="
+./array88/main.c
+./display/main.c
+./lib/array88.c
+./lib/array88.h
+./lib/delay.c
+./lib/delay.h
+./lib/display.c
+./lib/display.h
+./lib/hw.h
+"
+
+for f in $FILES; do
+  indent $f -brf -linux -l10000
+	rm $f~
+done
+
